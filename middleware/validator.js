3b93f96e12d37ce1029b6519a93319d761f2abb4
Adrienn <adrienntollas@gmail.com>
2020-04-28 18:36:04 +0200

    create validator

---
+++ middleware/validator.js
@@ -0,0 +1,46 @@
+const {body, validationResult} = require("express-validator");
+
+exports.validateInput = () => {
+    return[
+        body("email")
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("invalid email address"),
+
+        body("password")
+        .isLength({min: 6})
+        .withMessage("password is too short"),
+
+        body("firstName")
+        .notEmpty()
+        .exists()
+        .trim()
+        .escape()
+        .withMessage("Please give your first name"),
+
+        body("lastName")
+        .notEmpty()
+        .exists()
+        .trim()
+        .escape()
+        .withMessage("Please give your last name"),
+
+        (req,res,next) =>{
+            let errors = validationResult(req)
+
+            if(!errors.isEmpty()){
+                console.log(errors)
+                let err = errors.errors.map(er =>({
+                    [er.param]: er.msg
+                }))
+                res.json({
+                    status: 203,
+                    message: err
+                })
+            }
+            next()
+        }
+
+
+    ]
+}
