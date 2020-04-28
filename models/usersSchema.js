62f2bf56790c6c456df92341ce6074d677bf2358
Adrienn <adrienntollas@gmail.com>
2020-04-28 18:42:23 +0200

    install unique-validator, import it to userSchema

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
 const { Schema } = mongoose;
+const uniqueValidator = require("mongoose-unique-validator")
 
 const UserSchema = new Schema({
-   firstName: {type: String, required: true},
+   firstName: {type: String, required: true, unique:true},
    lastName: {type: String, required: true},
-   email: {type: String, required: true},
+   email: {type: String, required: true, unique:true},
    password: {type: String, required: true} 
 },
 {
@@ -20,5 +21,7 @@ const UserSchema = new Schema({
 UserSchema.virtual("fullname").get(function(){
     return `${this.firstName} ${this.lastName}`
 })
+
+UserSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model( "User", UserSchema);
